378f5d3397aa24c2ba846668045ef2fe89b767e4
kalanto <sandrewbooth@gmail.com>
2016-04-10 14:28:00 -0600

    code

--- Week3/day15/angular-drills/app4-api/service.js
+++ Week3/day15/angular-drills/app4-api/service.js
@@ -5,7 +5,7 @@ angular.module("appApi").service("serviceApi", function($http){
    this.getApi = function(){
       return $http({
          method: "GET",
-         url: "http://swapi.co/api/people/5/"
+         url: "http://swapi.co/api/people"
       }).then(function(response){
          console.log(response);
          return response.data;
