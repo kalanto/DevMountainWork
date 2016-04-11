a845978136489e54ba66e6ca571ddcb01a25b8a0
kalanto <sandrewbooth@gmail.com>
2016-04-11 15:15:26 -0600

    step 2

---
+++ Week4/day16/nbaRoutes/js/app.js
@@ -0,0 +1,14 @@
+var app = angular.module('nbaRoutes', ['ui.router']);
+
+app.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+
+    // routing configuration code
+    $stateProvider(
+      .state{
+
+      }
+   }
+
+});
---
+++ Week4/day16/nbaRoutes/js/teams/teamService.js
@@ -0,0 +1,38 @@
+var app = angular.module('nbaRoutes');
+
+app.service('teamService', function ($http, $q) {
+   this.addNewGame = function(gameObj){
+      var deferred = $q.defer();
+      var win = 0;
+      var losses = 0;
+      var url = 'https://api.parse.com/1/classes/' + gameObj.homeTeam;
+         if(parseInt("gamObj.homeTeamScore") >    parseInt("gameObj.opponentScore")){
+            gameObj.won = true;
+         }
+         else
+            {gamObj.won = false;}
+         $http({
+            method: 'POST',
+            url: url,
+            data: gameObj
+         }).then(function(response){
+         });
+         $http({
+            method: 'GET',
+            url: url
+         }).then(function(data){
+            var results = data.data.results;
+            for(var i = 0; i < results.length; i++){
+            if(results[i].won === true){
+               wins++;
+            }
+            else{losses++;}
+            results.wins = wins;
+            results.losses = losses;
+            deferred.resolve = results;
+         }
+
+         });
+         return deferred.promise;
+   };
+});
